d040fc1812115be364cef06188fe8650404cf91e
vijayakowsi <43548095+vijayakowsi@users.noreply.github.com>
2019-05-17 10:26:22 +0530

    Create sum of array.java

---
+++ sum of array.java
@@ -0,0 +1,25 @@
+/*package whatever //do not write package name here */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG {
+	public static void main (String[] args) {
+		//code
+		Scanner sc=new Scanner(System.in);
+		int t=sc.nextInt();
+		while(t-->0)
+		{
+		    int sum=0;
+		    int n=sc.nextInt();
+		    int arr[]=new int[n];
+		    for(int i=0;i<n;i++)
+		    {
+		        arr[i]=sc.nextInt();
+		        sum=sum+arr[i];
+		    }
+		    System.out.println(sum);
+		}
+	}
+}
